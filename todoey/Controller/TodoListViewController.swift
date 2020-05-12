170af25ee7e1a70d74befd299ad95b5282a7c938
MacOS <saeedadhami456@gmail.com>
2020-05-12 14:19:28 +0430

    persisting data using codable protocol  and plist

--- todoey/Controller/TodoListViewController.swift
+++ todoey/Controller/TodoListViewController.swift
@@ -12,73 +12,24 @@ class TodoListViewController: UITableViewController {
     
     
     var listArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(dataFilePath!)
         
+        loadItem()
         
-        let newItem = Item()
-        newItem.title = "icecream"
-        listArray.append(newItem)
+
         
-        let newItem1 = Item()
-        newItem1.title = "Milk"
-        listArray.append(newItem1)
-        
-        
-        let newItem2 = Item()
-        newItem2.title = "Cream"
-        listArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "ice"
-        listArray.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.title = "meat"
-        listArray.append(newItem4)
-        
-        let newItem5 = Item()
-        newItem5.title = "banana"
-        listArray.append(newItem5)
-        
-        let newItem6 = Item()
-        newItem6.title = "peach"
-        listArray.append(newItem6)
-        
-        let newItem7 = Item()
-        newItem7.title = "apple"
-        listArray.append(newItem7)
-        
-        let newItem8 = Item()
-        newItem8.title = "door"
-        listArray.append(newItem8)
-        
-        let newItem9 = Item()
-        newItem9.title = "table"
-        listArray.append(newItem9)
-        
-        let newItem10 = Item()
-        newItem10.title = "perfume"
-        listArray.append(newItem10)
-        
-        let newItem11 = Item()
-        newItem11.title = "orange"
-        listArray.append(newItem11)
-    
-        
-        if let items = defaults.array(forKey: "TodoList") as? [Item]{
-            listArray = items
-        }
     }
     
     //MARK : tableview datasource method
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("cell For Row At")
+        
         
         let item = listArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -100,7 +51,7 @@ class TodoListViewController: UITableViewController {
         print("did Selected Row At")
         
         listArray[indexPath.row].done = !listArray[indexPath.row].done
-        print(listArray[indexPath.row].title)
+        saveItem()
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -118,10 +69,7 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             
             self.listArray.append(newItem)
-            
-            self.defaults.set(self.listArray, forKey: "TodoList")
-            
-            self.tableView.reloadData()
+            self.saveItem()
             
         }
         
@@ -134,6 +82,39 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    
+    // MARK - model manupulation method
+    
+    func saveItem(){
+        
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(listArray)
+            try data.write(to: dataFilePath!)
+            print("saveItem for data : \(data)")
+            print("saveItem for encoder :\(encoder)")
+            
+        }catch{
+            print("we have error for encoding \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func loadItem(){
+        
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            print("loadItem for decoder : \(decoder)")
+            print("loadItem for data : \(data)")
+            do{
+                listArray = try decoder.decode([Item].self, from: data)
+            
+            }catch{
+                print("we have errror decoding \(error)")
+            }
+        }
     }
 }
 
